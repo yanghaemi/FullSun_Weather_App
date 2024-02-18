01a01cea8e5eaebc766440905c4161c7085e6e5b
yanghaemi <heau0105@naver.com>
2024-02-19 04:58:35 +0900

    create: expo 앱 실행화면 test

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading(){
+    return (
+    <View style={styles.container}>
+        <Text style={styles.text}>해짜니 와떠여 ~~</Text>
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: "white"
+    },
+    text: {
+        color: "black",
+        fontSize: 30
+    }
+});
